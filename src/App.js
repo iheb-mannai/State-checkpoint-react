d2bad2d6c7e941252a73f9d7d3bfe14231dffb83
iheb-mannai <ihebmannai6@gmail.com>
2021-04-02 00:04:27 +0200

    state checkpoint

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react' ;
+import './App.css' ;
+import Profile from './components/Profile'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    person : {
+      fullName : 'iheb mannai',
+      bio : 'he is smart', 
+      imgSrc : '/28471474_1933061906722798_6846834042043856043_n.jpg', 
+      profession : 'Developper',
+      phone : '99 98 92 43',
+      email: 'ihebmn6@gmail.com'
+    },
+    show : true ,
+    
+  }
+  render() {
+    return (
+      <div>
+        <button type="button" className="btn btn-secondary" style={{width: '10%', marginLeft: '45%' , marginTop: '10px'}} onClick={() => this.setState({show : !this.state.show})}>Click</button>
+    
+{this.state.show ? <Profile/> : null}
+     </div>
+
+
+
+    
+
+  
+    )
+  }
 }
 
-export default App;
